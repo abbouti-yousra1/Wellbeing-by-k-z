922e3828c751c50540838ae217a1b3b08f2f5e00
yousra abbouti <abboutiyousra@gmail.com>
2025-09-01 23:45:50 +0100

    created colors.ts for app colors

---
+++ frontend/src/constants/Colors.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+  // Primary Palette
+  softLavender: '#ECD5E5', // Backgrounds, subtle accents
+  vibrantPlum: '#AA4586', // Primary buttons, highlights
+  deepPurple: '#4E0250', // Headers, bold accents
+  warmYellow: '#E6E49F', // Positive actions, highlights
+  charcoal: '#25291C', // Backgrounds, dark mode
+
+  // Text Colors
+  textPrimary: '#000000', // Main text
+  textSecondary: '#4B4B4B', // Secondary text, captions
+  textPlaceholder: '#8A8A8A', // Placeholders, disabled states
+
+  // Additional Greys for UI
+  lightGrey: '#D3D3D3', // Borders, dividers
+  mediumGrey: '#6B7280', // Secondary buttons, icons
+
+  //light bg
+  lightBg: '#f5f2f2ff',
+} as const;
